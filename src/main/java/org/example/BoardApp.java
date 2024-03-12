c2e493730647609d198e758f634022d4e4e6c490
Dumbbell04 <Dumbbelloper04@gmail.com>
2024-03-12 12:23:29 +0900

    BoardManager

--- src/main/java/org/example/BoardApp.java
+++ src/main/java/org/example/BoardApp.java
@@ -1,21 +1,103 @@
 package org.example;
 
+import java.security.PrivateKey;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.PrimitiveIterator;
 import java.util.Scanner;
 
+class BoardManager {
+    private ArrayList<Post> postList;
+    private int count;
+
+    public BoardManager(){
+        postList = new ArrayList<>();
+        count = 0;
+    }
+     public void addPost(String title, String body){ //Post 생성
+        count++;
+        postList.add(new Post(count, title, body, LocalDateTime.now())); //고유번호와 제목, 내용, 입력시간으로 새 Post를 생성
+         System.out.println("게시물이 등록되었습니다."); //게시물 등록
+     }
+     public List<Post> getPostList(){
+        return postList;
+     }
+     public void updatePost(Post post, String title, String body){
+        post.setTitle(title);
+        post.setBody(body);
+        post.setDateTime(LocalDateTime.now());
+        System.out.println(post.getNumber() + "번 게시물이 수정되었습니다.");
+     }
+     public void deletePost(int postNumber){
+         for (int i = 0; i < postList.size(); i++) {
+             if (postList.get(i).getNumber() == postNumber) {
+                 postList.remove(i);
+                 System.out.println(postNumber + "번 게시물이 삭제되었습니다.");
+                 return; // 삭제 완료 후 메서드 종료
+             }
+         }
+         System.out.println("존재하지 않는 게시물 번호입니다.-delete");
+     }
+     public void showPost(Post post){
+         System.out.println("========================");
+         System.out.println("번호 : " + post.getNumber());
+         System.out.println("제목 : " + post.getTitle());
+         System.out.println("내용 : " + post.getBody());
+         System.out.println("등록 날짜 : " + post.getDateTime());
+         System.out.println("========================");
+     }
+}
+class Post {
+    private int number; //BoardManager에게 받은 고유번호
+    private String title;
+    private String body;
+    private LocalDateTime dateTime;
+
+    public Post(int number, String title, String body, LocalDateTime dateTime){ //post 생성
+        this.number = number;
+        this.title = title;
+        this.body = body;
+        this.dateTime = dateTime;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
 public class BoardApp {
     public void run(){
         Scanner scan = new Scanner(System.in);
-
-        //변수에는 하나의 값만 저장 가능하므로 여러개의 게시물을 저장하려면 ArrayList를 사용한다.
-        ArrayList<String> titleList = new ArrayList<>(); //제목 배열
-        ArrayList<String> bodyList = new ArrayList<>(); //내용 배열
-        ArrayList<Integer> numList = new ArrayList<>(); //고유번호 배열
-        ArrayList<String> timeList = new ArrayList<>(); //날짜 배열
-
-        int count = 0; //고유번호
+        BoardManager boardManager = new BoardManager();
 
         //반복 횟수를 정할 수 없음. => 무한 반복 (while) 구조
         while(true) { //반복 조건이 true이기 때문에 무한 반복 구조
@@ -30,113 +112,74 @@ public class BoardApp {
             else if (cmd.equals("add")) {
                 System.out.print("제목을 입력하세요 : "); //제목 입력받음
                 String title = scan.nextLine();
-                titleList.add(title);
 
                 System.out.print("내용을 입력하세요 : "); //내용 입력받음
                 String body = scan.nextLine();
-                bodyList.add(body);
 
-                System.out.println("게시물이 등록되었습니다.");
-
-                count++; //고유번호 지정
-                numList.add(count); //고유번호 리스트에 입력
-
-                LocalDateTime dateTime = LocalDateTime.now();
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"); //형식 생성
-                String formattedDateTime = dateTime.format(formatter); //변환된 형식을 적용
-                timeList.add(formattedDateTime); //형식으로 변환된 시간을 리스트에 추가
-
+                boardManager.addPost(title, body); //제목과 내용을 입력받고 boardManager에게 addPost작업을 시킴
             }
             else if (cmd.equals("list")) {
                 System.out.println("========================");
-                for (int i = 0; i < numList.size(); i++) {
-
-                    int num1 = numList.get(i);
-                    System.out.println("번호 : " + num1);
-
-                    String title = titleList.get(i);
-                    System.out.println("제목 : " + title);
-
+                List<Post> posts = boardManager.getPostList(); //Post의 리스트를 가져온다.
+                for (Post post : posts) {
+                    System.out.println("번호 : " + post.getNumber());
+                    System.out.println("제목 : " + post.getTitle());
                     System.out.println("========================");
                 }
             }
+
+
             else if (cmd.equals("update")) {
-                int index = 0;
-                int forcount = 0;
+                Post indexPost = null;
+                boolean postNum = false;
                 System.out.print("수정할 게시물 번호 : ");
                 int num = Integer.parseInt(scan.nextLine());
+                List<Post> posts = boardManager.getPostList();
 
-                //입력받은 번호와 등록된 번호가 같은 인덱스를 찾는다.
-                //for문이 한번이라도 실행되었는지 확인한다.
-                for (int i = 0; i < numList.size(); i++) {
-                    if (num == numList.get(i)) {
-                        index = i;
-                        forcount++;
+                for(Post post : posts){ //리스트에 있는지 찾는다
+                    if(post.getNumber() == num){
+                        indexPost = post;
+                        postNum = true;
                     }
                 }
-                if (forcount > 0) { //for문이 한번이라도 실행되었다면 인덱스를 찾아 수정한다.
-                    System.out.print("제목 : ");
+
+                if(postNum){ //있다
+                    System.out.print("새 제목을 입력하세요 : ");
                     String title = scan.nextLine();
-                    titleList.set(index, title);
 
-                    System.out.print("내용 : ");
+                    System.out.print("새 내용을 입력하세요 : ");
                     String body = scan.nextLine();
-                    bodyList.set(index, body);
 
-                    System.out.println(num + "번 게시물이 수정되었습니다.");
-
-                    //수정 시간 update
-                    LocalDateTime dateTime = LocalDateTime.now();
-                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"); //형식 생성
-                    String formattedDateTime = dateTime.format(formatter); //변환된 형식을 적용
-                    timeList.set(index, formattedDateTime); //인덱스에 변환된 시간 수정
-                } else { //for문이 한번도 실행되지 않았거나, 그 외에는 모두 없는 게시물이다.
-                    System.out.println("없는 게시물 번호입니다.");
+                    boardManager.updatePost(indexPost, title, body);
+                }else {
+                    System.out.println("존재하지 않는 게시물입니다.");
                 }
             }
+
+
             else if (cmd.equals("delete")) {
-                int index = 0;
-                int forcount = 0;
                 System.out.print("삭제할 게시물 번호 : ");
                 int num = Integer.parseInt(scan.nextLine());
-
-                for (int i = 0; i < numList.size(); i++) {
-                    if (num == numList.get(i)) {
-                        index = i;
-                        forcount++;
-                    }
-                }
-                if (forcount > 0) {
-                    titleList.remove(index);
-                    bodyList.remove(index);
-                    numList.remove(index);
-                    timeList.remove(index);
-                    System.out.println(num + "번 게시물이 삭제되었습니다.");
-
-                } else {
-                    System.out.println("없는 게시물입니다.");
-                }
+                boardManager.deletePost(num);
             }
+
+
             else if (cmd.equals("detail")) {
-                int index = 0;
-                int forcount = 0;
+                Post indexPost = null;
+                boolean postNum = false;
                 System.out.print("상세보기 할 게시물 번호 : ");
                 int num = Integer.parseInt(scan.nextLine());
+                List<Post> posts = boardManager.getPostList();
 
-                for (int i = 0; i < numList.size(); i++) {
-                    if (num == numList.get(i)) {
-                        index = i;
-                        forcount++;
+                for (Post post : posts) {
+                    if (num == post.getNumber()) {
+                        indexPost = post;
+                        postNum = true;
                     }
                 }
-                if (forcount > 0) {
-                    System.out.println("========================");
-                    System.out.println("번호 : " + numList.get(index));
-                    System.out.println("제목 : " + titleList.get(index));
-                    System.out.println("내용 : " + bodyList.get(index));
-                    System.out.println("등록 날자 : " + timeList.get(index));
-                    System.out.println("========================");
-                } else { //없으면 출력
+                if (postNum) {
+                    boardManager.showPost(indexPost);
+                } else {
                     System.out.println("존재하지 않는 게시물입니다.");
                 }
             }
